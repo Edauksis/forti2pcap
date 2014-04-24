121faeb8fa99a5bc64fa3b2d40b711d4a8929a85
Esteban Dauksis <esteban@botijo.mju.es>
2014-04-24 14:42:46 +0200

    Small changes commit

--- forti2pcap.bash
+++ forti2pcap.bash
@@ -1,21 +1,31 @@
 #!/bin/bash
 # Convert Fortigate's '''diagnose sniffer packet any "host IP" 6 0 a''' output 2 pcap
-# undebug - esteban@dauksis.com
+# undebug - esteban(at)dauksis.com
 
-F_IN=$1
+if [ ! -f $(which text2pcap) ] 
+then  
+	echo "Install Wireshark!"
+	exit 1
+fi
 
-for Interface in $(cat $F_IN | grep -E ^[0-9]\{4\}\-[0-9]\{2\}\-[0-9]\{2\} | awk '{ print $3 }' | sort | uniq)
-do
-	echo "Processing $F_IN.$Interface.pcap"
-	cat $F_IN | sed -e /.*"$Interface".*/,/^.$/\!d | \
-	sed -e 's/\ \([0-9a-fA-F]\{2\}\)\([0-9a-fA-F]\{2\}\)\ /\ \1\ \2\ /g' | \
-	sed -e 's/\ \([0-9a-fA-F]\{2\}\)\([0-9a-fA-F]\{2\}\)\ /\ \1\ \2\ /g' | \
-	sed -e 's/\ \([0-9a-fA-F]\{2\}\)\([0-9a-fA-F]\{2\}\)\t/\ \1\ \2\ \t/g' | \
-	sed -e 's/^0x/00/' | \
-	sed -e 's/\(^[0-9]\{4\}\-[0-9]\{2\}\-[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\.[0-9]\{6\}\ \).*/\1/' | \
-	text2pcap -t "%Y-%m-%d %H:%M:%S." - $F_IN.$Interface.pcap 2>&1 | tail -1
+if [ $# -ne '1' ]
+then
+	echo "No file!"
+	echo "Usage: $0	diagnose_sniffer_packet.output"
+else
+	F_IN=$1
 
-done
-echo -e "First packet: $(cat $F_IN | grep  -Eo ^[0-9]\{4\}\-[0-9]\{2\}\-[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\.[0-9]\{6\}\ | head -1)"
-echo -e "Last packet: $(cat $F_IN | grep  -Eo ^[0-9]\{4\}\-[0-9]\{2\}\-[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\.[0-9]\{6\}\ | tail -1)\n"
-
+	for Interface in $(cat $F_IN | grep -E ^[0-9]\{4\}\-[0-9]\{2\}\-[0-9]\{2\} | awk '{ print $3 }' | sort | uniq)
+	do
+		echo "Processing $F_IN.$Interface.pcap"
+		cat $F_IN | sed -e /.*"$Interface".*/,/^.$/\!d | \
+		sed -e 's/\ \([0-9a-fA-F]\{2\}\)\([0-9a-fA-F]\{2\}\)\ /\ \1\ \2\ /g' | \
+		sed -e 's/\ \([0-9a-fA-F]\{2\}\)\([0-9a-fA-F]\{2\}\)\ /\ \1\ \2\ /g' | \
+		sed -e 's/\ \([0-9a-fA-F]\{2\}\)\([0-9a-fA-F]\{2\}\)\t/\ \1\ \2\ \t/g' | \
+		sed -e 's/^0x/00/' | \
+		sed -e 's/\(^[0-9]\{4\}\-[0-9]\{2\}\-[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\.[0-9]\{6\}\ \).*/\1/' | \
+		text2pcap -t "%Y-%m-%d %H:%M:%S." - $F_IN.$Interface.pcap 2>&1 | tail -1
+	done
+	echo -e "First packet: $(cat $F_IN | grep  -Eo ^[0-9]\{4\}\-[0-9]\{2\}\-[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\.[0-9]\{6\}\ | head -1)"
+	echo -e "Last packet: $(cat $F_IN | grep  -Eo ^[0-9]\{4\}\-[0-9]\{2\}\-[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\.[0-9]\{6\}\ | tail -1)\n"
+fi
